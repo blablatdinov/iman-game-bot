7ba8e5df403010a8dd0e3485b038ffdf08800a5b
Almaz <a.ilaletdinov@yandex.ru>
2020-09-19 11:02:46 -0400

    return some functions

--- bot_init/service.py
+++ bot_init/service.py
@@ -1,3 +1,8 @@
+from time import sleep
+
+from django.conf import settings
+from telebot import TeleBot
+
 from bot_init.models import Subscriber
 from game.models import MembersGroup
 
@@ -11,3 +16,15 @@ def registration_subscriber(chat_id: int, text: str):
     subscriber, created = Subscriber.objects.get_or_create(tg_chat_id=chat_id)
     pk = get_primary_key_from_start_message(text)
     subscriber.members_group = MembersGroup.objects.get(pk=pk)
+
+
+def get_tbot_instance():
+    return TeleBot(settings.TG_BOT.token)
+
+
+def update_webhook(host=f'{settings.TG_BOT.webhook_host}/{settings.TG_BOT.token}'):
+    """Обновляем webhook"""
+    tbot = get_tbot_instance()
+    tbot.remove_webhook()
+    sleep(1)
+    web = tbot.set_webhook(host)
