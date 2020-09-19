59f237f0015dda6cd4e64eceefaf51643036e977
Almaz <a.ilaletdinov@yandex.ru>
2020-09-19 17:50:47 +0300

    add service for bind subscriber with group

--- bot_init/service.py
+++ bot_init/service.py
@@ -1,7 +1,13 @@
 from bot_init.models import Subscriber
+from game.models import MembersGroup
 
 
-def registration_subscriber(chat_id: int):
+def get_primary_key_from_start_message(text: str) -> int:
+    return int(text[7:])
+
+
+def registration_subscriber(chat_id: int, text: str):
     """Логика сохранения подписчика"""
     subscriber, created = Subscriber.objects.get_or_create(tg_chat_id=chat_id)
-    ...
+    pk = get_primary_key_from_start_message(text)
+    subscriber.members_group = MembersGroup.objects.get(pk=pk)
