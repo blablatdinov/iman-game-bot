de0dc236c8dda8d2f0e37505fe6420d66149c6a0
Almaz <a.ilaletdinov@yandex.ru>
2020-09-29 12:16:31 +0300

    add task for asking

--- game/tasks.py
+++ game/tasks.py
@@ -2,9 +2,14 @@ from __future__ import absolute_import, unicode_literals
 from celery.task import periodic_task
 from celery.schedules import crontab
 
-from game.service import send_daily_tasks
+from game.service import send_daily_tasks, ask_about_task
 
 
-@periodic_task(run_every=(crontab(hour=7, minute=0)), name='mailing')
-def mailing():
+@periodic_task(run_every=(crontab(hour=7, minute=0)), name='send_daily_tasks')
+def send_daily_tasks():
     send_daily_tasks()
+
+
+@periodic_task(run_every=(crontab(hour=7, minute=0)), name='ask_about_task')
+def ask_about_task():
+    ask_about_task()
