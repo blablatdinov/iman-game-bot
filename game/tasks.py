5517f711c18913440ed734149b1c231ed3eb14fe
Almaz <a.ilaletdinov@yandex.ru>
2020-09-29 20:23:00 +0300

    add celery task for clean asks

--- game/tasks.py
+++ game/tasks.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 from celery.task import periodic_task
 from celery.schedules import crontab
 
-from game.service import send_daily_tasks, ask_about_task
+from game.service import send_daily_tasks, ask_about_task, clean_ask
 
 
 @periodic_task(run_every=(crontab(hour=7, minute=0)), name='send_daily_tasks')
@@ -13,3 +13,8 @@ def celery_send_daily_tasks():
 @periodic_task(run_every=(crontab(hour=20, minute=0)), name='ask_about_task')
 def celery_ask_about_task():
     ask_about_task()
+
+
+@periodic_task(run_every=(crontab(hour=24, minute=0)), name='clean_asks')
+def celery_clean_ask():
+    clean_ask()
