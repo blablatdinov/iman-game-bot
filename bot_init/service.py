1580a172d901d30e068592ae26a6806fd56e1bf0
Almaz <a.ilaletdinov@yandex.ru>
2020-11-17 12:59:01 +0300

    separate some actions

--- bot_init/service.py
+++ bot_init/service.py
@@ -78,75 +78,80 @@ def text_message_service(chat_id: int, text: str):
         return Answer(text, keyboard=keyboard, chat_id=chat_id)
 
 
+def get_args(text):
+    return eval(re.search(r'\(.+\)', text).group(0))
+
+
+TIME_LIMITS_FOR_SELECT_TASKS = (0, 8)
+
+
+def set_task_to_selected_or_unselected(record_daily_task_id, level, chat_id):
+    record_daily_task = RecordDailyTask.objects.get(pk=record_daily_task_id)
+    if not(TIME_LIMITS_FOR_SELECT_TASKS[0] <= (timezone.now().hour + 3) % 24 <= TIME_LIMITS_FOR_SELECT_TASKS[1]):
+        return
+    record_task_group = record_daily_task.group
+    task_type = record_daily_task.task.task_type
+    record_daily_task.switch(level)
+    tasks = record_task_group.daily_tasks_records.filter(task__task_type=task_type)
+    # TODO выхардкодить индексы
+    text = f"{tasks[0].task.get_task_type_display()}\n\n" \
+           f"1) {tasks[0].task.text}\n" \
+           f"2) {tasks[1].task.text}\n" \
+           f"3) {tasks[2].task.text}\n"
+    return Answer(text, keyboard=translate_tasks_in_keyboard(tasks), chat_id=chat_id)
+
+
+def set_task_to_done(task_id, task_status, next_tasks_list, chat_id, message_id):
+    if task_status:
+        task = RecordDailyTask.objects.get(pk=task_id)
+        task.set_done()
+    if len(next_tasks_list) == 0:
+        subscriber = Subscriber.objects.get(tg_chat_id=chat_id)
+        text = AdminMessage.objects.get(key="other_day_survey").text
+        if subscriber.day == 1:
+            subscriber.day = 2
+            subscriber.save()
+        else:
+            ...
+        tg_delete_message(chat_id=chat_id, message_id=message_id)
+        Answer(text, keyboard=get_default_keyboard(), chat_id=chat_id).send()
+        return
+    text, keyboard = ask_single_task(next_tasks_list)
+    return Answer(text, keyboard=keyboard, chat_id=chat_id)
+
+
+def begin_survey(value, begin_question_pk, chat_id):
+    set_points(chat_id, begin_question_pk, value)
+    if begin_question_pk == 30:
+        admin_message = AdminMessage.objects.get(pk=3)
+        text = admin_message.text
+        keyboard = get_acquaintance_next_keyboard(3)
+        sub = Subscriber.objects.get(tg_chat_id=chat_id)
+        sub.step = "ask_sub_purpose"
+        sub.save()
+        return Answer(text, keyboard=keyboard, chat_id=chat_id)
+    answer = get_next_question(begin_question_pk)
+    answer.chat_id = chat_id
+    return answer
+
+
 def handle_query_service(chat_id: int, text: str, message_id: int, message_text: str, call_id: int):
     """Обработка нажатий на inline кнопку"""
     # TODO побить функцию
     log.info(f"{chat_id=} {text}")
-    TIME_LIMITS_FOR_SELECT_TASKS = (0, 8)
-    print((timezone.now().hour + 3) % 24)
-    if "set_to_selected" in text and TIME_LIMITS_FOR_SELECT_TASKS[0] <= (timezone.now().hour + 3) % 24 <= TIME_LIMITS_FOR_SELECT_TASKS[1]:
-        print('wow')
-        record_daily_task_id, level = eval(re.search(r'\(.+\)', text).group(0))
-        # TODO создать отдельную функцию, чтобы доставать аргументы
-        record_daily_task = RecordDailyTask.objects.get(pk=record_daily_task_id)
-        record_task_group = record_daily_task.group
-        task_type = record_daily_task.task.task_type
-        record_daily_task.switch()
-        record_daily_task.complexity = level
-        record_daily_task.save()
-        tasks = record_task_group.daily_tasks_records.filter(task__task_type=task_type)
-        # TODO выхардкодить индексы
-        text = f"{tasks[0].task.get_task_type_display()}\n\n" \
-               f"1) {tasks[0].task.text}\n" \
-               f"2) {tasks[1].task.text}\n" \
-               f"3) {tasks[2].task.text}\n"
-        return Answer(text, keyboard=translate_tasks_in_keyboard(tasks), chat_id=chat_id)
-    elif "set_to_unselected" in text and TIME_LIMITS_FOR_SELECT_TASKS[0] <= (timezone.now().hour + 3) % 24 <= TIME_LIMITS_FOR_SELECT_TASKS[1]:
-        print('owo')
-        record_daily_task_id, level = eval(re.search(r'\(.+\)', text).group(0))
-        record_daily_task = RecordDailyTask.objects.get(pk=record_daily_task_id)
-        record_task_group = record_daily_task.group
-        task_type = record_daily_task.task.task_type
-        record_daily_task.switch()
-        record_daily_task.complexity = 0
-        record_daily_task.save()
-        tasks = record_task_group.daily_tasks_records.filter(task__task_type=task_type)
-        text = f"{tasks[0].task.get_task_type_display()}\n\n" \
-               f"1) {tasks[0].task.text}\n" \
-               f"2) {tasks[1].task.text}\n" \
-               f"3) {tasks[2].task.text}\n"
-        return Answer(text, keyboard=translate_tasks_in_keyboard(tasks), chat_id=chat_id)
+    if "set_to_selected" in text or "set_to_unselected" in text:
+        record_daily_task_id, level = get_args(text)
+        answer = set_task_to_selected_or_unselected(record_daily_task_id, level, chat_id)
+        return answer
     elif "set_to_done" in text or "settodone" in text:
-        task_id, task_status, next_tasks_list = eval(re.search(r'\(.+\)', text).group(0))
-        if task_status:
-            task = RecordDailyTask.objects.get(pk=task_id)
-            task.set_done()
-        if len(next_tasks_list) == 0:
-            subscriber = Subscriber.objects.get(tg_chat_id=chat_id)
-            text = AdminMessage.objects.get(key="other_day_survey").text
-            if subscriber.day == 1:
-                subscriber.day = 2
-                subscriber.save()
-            else:
-                ...
-            tg_delete_message(chat_id=chat_id, message_id=message_id)
-            Answer(text, keyboard=get_default_keyboard(), chat_id=chat_id).send()
-            return
-        text, keyboard = ask_single_task(next_tasks_list)
-        return Answer(text, keyboard=keyboard, chat_id=chat_id)
+        task_id, task_status, next_tasks_list = get_args(text)
+        answer = set_task_to_done(
+            task_id, task_status, next_tasks_list, chat_id, message_id
+        )
+        return answer
     elif "begin_survey" in text:
         value, begin_question_pk = eval(re.search(r'\(.+\)', text).group(0))
-        set_points(chat_id, begin_question_pk, value)
-        if begin_question_pk == 30:
-            admin_message = AdminMessage.objects.get(pk=3)
-            text = admin_message.text
-            keyboard = get_acquaintance_next_keyboard(3)
-            sub = Subscriber.objects.get(tg_chat_id=chat_id)
-            sub.step = "ask_sub_purpose"
-            sub.save()
-            return Answer(text, keyboard=keyboard, chat_id=chat_id)
-        answer = get_next_question(begin_question_pk)
-        answer.chat_id = chat_id
+        answer = begin_survey(value, begin_question_pk, chat_id)
         return answer
     elif "acquaintance" in text:
         step_num = int(re.search(r'\d+', text).group(0))
